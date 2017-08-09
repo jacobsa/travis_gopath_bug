19306a307d654e12b4a9a9040f79a9e107667c0b
Aaron Jacobs <jacobsa@google.com>
2017-08-09 10:58:19 +1000

    stat goroot

--- foo_test.go
+++ foo_test.go
@@ -7,6 +7,12 @@ import (
 )
 
 func TestFoo(t *testing.T) {
+	// Print some debugging info.
 	t.Logf("GOROOT: %q", runtime.GOROOT())
 	t.Logf("os.Environ: %v", os.Environ())
+
+	// Attempt to stat GOROOT.
+	if _, err := os.Stat(runtime.GOROOT()); err != nil {
+		t.Errorf("failed to stat GOROOT (%q): %v", runtime.GOROOT(), err)
+	}
 }
